9c18f5b5cec67b3f159cbbdcef267b5a00aa4714
shadowevil <Abdulla@ABDULLA-PC>
2026-04-19 19:06:43 +0300

    1.0.5

---
+++ StatUpdate.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace fasterPace
+{
+    [HarmonyPatch(typeof(AdventureController), "Start")]
+    internal static class Patch_AdventureController_Start_FasterStatRefresh
+    {
+        private const float AdventureStatRefreshInterval = 0.05f;
+
+        [HarmonyPostfix]
+        private static void Postfix(AdventureController __instance)
+        {
+            if (__instance == null)
+                return;
+
+            __instance.CancelInvoke("updateAdventureStats");
+            __instance.CancelInvoke("displayEnemyStats");
+
+            __instance.InvokeRepeating("updateAdventureStats", 0f, AdventureStatRefreshInterval);
+            __instance.InvokeRepeating("displayEnemyStats", 0f, AdventureStatRefreshInterval);
+        }
+    }
+}
